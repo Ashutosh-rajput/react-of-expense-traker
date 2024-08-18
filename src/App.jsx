f54664407e6169a3417a932ae918e5ac63eadb8d
ashutosh rajput <rajputashutosh81@gmail.com>
2024-08-19 01:05:03 +0530

    Complete and puched github

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import Dashboard from './components/Dashboard';
+import './App.css'
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/login" element={<LoginPage />} /> */}
+        
+        <Route path="/dashboard" element={<Dashboard />} />
+        
+        
+        
+        <Route path="/" element={<LoginPage />} />
+        
+       
+      </Routes>
+    </Router>
+  );
+};
+export default App;
---
+++ src/services/user-service.js
@@ -0,0 +1,7 @@
+// import { myAxios } from "./helper";
+
+// const signUp=(user)=>{
+//     return myAxios
+//        .post('/user/create-user')
+//        .then((response)=>response.json());
+// }; 
